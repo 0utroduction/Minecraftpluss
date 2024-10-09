570c6d1d6ff46e354eeb46018292f0a1398a5180
VotoxPlayz <phoen@10.49.101.104>
2024-10-09 15:43:12 +1100

    apple slices

--- src/main/java/net/mcreator/minecraftplus/MinecraftplusMod.java
+++ src/main/java/net/mcreator/minecraftplus/MinecraftplusMod.java
@@ -17,6 +17,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
+import net.mcreator.minecraftplus.init.MinecraftplusModTabs;
 import net.mcreator.minecraftplus.init.MinecraftplusModItems;
 
 import java.util.function.Supplier;
@@ -40,6 +41,8 @@ public class MinecraftplusMod {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
 		MinecraftplusModItems.REGISTRY.register(bus);
+
+		MinecraftplusModTabs.REGISTRY.register(bus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
--- src/main/java/net/mcreator/minecraftplus/init/MinecraftplusModItems.java
+++ src/main/java/net/mcreator/minecraftplus/init/MinecraftplusModItems.java
@@ -10,12 +10,12 @@ import net.minecraftforge.registries.DeferredRegister;
 
 import net.minecraft.world.item.Item;
 
-import net.mcreator.minecraftplus.item.ParlajeetItem;
+import net.mcreator.minecraftplus.item.AppleSliceItem;
 import net.mcreator.minecraftplus.MinecraftplusMod;
 
 public class MinecraftplusModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MinecraftplusMod.MODID);
-	public static final RegistryObject<Item> PARLAJEET = REGISTRY.register("parlajeet", () -> new ParlajeetItem());
+	public static final RegistryObject<Item> APPLE_SLICE = REGISTRY.register("apple_slice", () -> new AppleSliceItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
