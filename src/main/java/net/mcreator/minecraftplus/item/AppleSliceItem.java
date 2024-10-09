01361dc89d61591eae02c52b53f2a38a468d0a36
0utroduction <Aarav Gupta@10.49.101.105>
2024-10-09 15:53:50 +1100

    Made apple slice texture

--- src/main/java/net/mcreator/minecraftplus/item/AppleSliceItem.java
+++ src/main/java/net/mcreator/minecraftplus/item/AppleSliceItem.java
@@ -1,6 +1,15 @@
 
 package net.mcreator.minecraftplus.item;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+
 public class AppleSliceItem extends Item {
 	public AppleSliceItem() {
 		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON).food((new FoodProperties.Builder()).nutrition(2).saturationMod(1f).build()));
