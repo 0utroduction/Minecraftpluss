d3b27dbe940f521e7425d3cadf5ebada173f393f
0utroduction <Aarav Gupta@10.49.101.105>
2024-10-11 10:25:23 +1100

    ruby when eaten gives xp

--- src/main/java/net/mcreator/minecraftplus/item/RubyItem.java
+++ src/main/java/net/mcreator/minecraftplus/item/RubyItem.java
@@ -6,8 +6,11 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.InteractionHand;
+
+import net.mcreator.minecraftplus.procedures.RubyPlayerFinishesUsingItemProcedure;
 
 public class RubyItem extends Item {
 	public RubyItem() {
@@ -24,5 +27,15 @@ public class RubyItem extends Item {
 		InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
 		entity.startUsingItem(hand);
 		return ar;
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = super.finishUsingItem(itemstack, world, entity);
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		RubyPlayerFinishesUsingItemProcedure.execute(entity);
+		return retval;
 	}
 }
