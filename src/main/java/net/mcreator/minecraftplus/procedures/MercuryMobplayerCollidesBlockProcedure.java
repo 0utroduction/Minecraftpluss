724f82c4e74f196b2e88b347204e5d61f75dbfcb
0utroduction <Aarav Gupta@10.49.101.105>
2024-10-11 13:08:28 +1100

    mercury upd - change to mercury poisoning new t4hing it damages

--- src/main/java/net/mcreator/minecraftplus/procedures/MercuryMobplayerCollidesBlockProcedure.java
+++ src/main/java/net/mcreator/minecraftplus/procedures/MercuryMobplayerCollidesBlockProcedure.java
@@ -10,6 +10,6 @@ public class MercuryMobplayerCollidesBlockProcedure {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
-			_entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 100, 0));
+			_entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 1, 0));
 	}
 }
